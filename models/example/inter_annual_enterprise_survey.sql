67701d3d92be334ff95a73d4f4f4efa50b89e74c
Balakumar-cloudside <119562666+Balakumar-cloudside@users.noreply.github.com>
2023-10-30 15:47:58 +0530

    Update inter_annual_enterprise_survey.sql

--- models/example/inter_annual_enterprise_survey.sql
+++ models/example/inter_annual_enterprise_survey.sql
@@ -4,13 +4,13 @@ with
 inter_survey as (
 SELECT
      SUM(CAST(t2.value AS INT64)) AS Total_Value,
-     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey01'), except=["id","value"]) }},
-     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey02'), except=["id","value"]) }}
+     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey02'), except=["id","value"]) }},
+     {{ dbt_utils.star(from=ref('stg_annual_enterprise_survey01'), except=["id","value"]) }}
         
 FROM
-     {{ ref('stg_annual_enterprise_survey02') }} AS t2
+     {{ ref('stg_annual_enterprise_survey01') }} AS t2
 RIGHT JOIN
-     {{ ref('stg_annual_enterprise_survey01') }} AS t1
+     {{ ref('stg_annual_enterprise_survey02') }} AS t1
 ON
     t2.id = t1.id
 WHERE
